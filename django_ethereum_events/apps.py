afc94c1a1ebf14dbb393234233055915132a9fb8
Aleksey Zhukov <drdaeman@drdaeman.pp.ru>
2017-12-18 20:06:02 +0300

    Fix for the previous commit (Celery app removal)
    Haven't paid enough attention and missed what ready method does.
    Removed the code. Libraries shouldn't do this - it's main project responsibility.

--- django_ethereum_events/apps.py
+++ django_ethereum_events/apps.py
@@ -4,8 +4,3 @@ from django.conf import settings
 
 class EthereumEventsConfig(AppConfig):
     name = 'django_ethereum_events'
-
-    def ready(self):
-        super(EthereumEventsConfig, self).ready()
-        app.config_from_object('django.conf:settings')
-        app.autodiscover_tasks(lambda: settings.INSTALLED_APPS, force=True)
