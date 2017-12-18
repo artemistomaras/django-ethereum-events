a64338d8501e3aadf31e297a81325c08ad5463d0
Aleksey Zhukov <drdaeman@drdaeman.pp.ru>
2017-12-18 21:05:48 +0300

    Rather than iterating blocks, call eth.getLogs
    We really don't want to do over 4.7M requests on the first run and manually
    filter every transaction in there. Let's use `eth_getLogs` call instead.

--- django_ethereum_events/event_listener.py
+++ django_ethereum_events/event_listener.py
@@ -3,6 +3,7 @@ from .models import Daemon
 from .singleton import Singleton
 from .web3_service import Web3Service
 from .exceptions import UnknownBlock
+from django.conf import settings
 from django.utils.module_loading import import_string
 
 
@@ -18,14 +19,14 @@ class EventListener(metaclass=Singleton):
         """Retrieve the blocks that have not been processed.
 
         Returns:
-            List containing the unprocessed block numbers.
+            An iterable of (from, to) tuples containing the unprocessed block numbers.
         """
-        blocks_to_process = []
         daemon = Daemon.get_solo()
         current = self.web3.eth.blockNumber
+        step = getattr(settings, "ETHEREUM_LOGS_BATCH_SIZE", 10000)
         if daemon.block_number < current:
-            blocks_to_process = range(daemon.block_number + 1, current + 1)
-        return blocks_to_process
+            return ((r, min(current, r + step)) for r in range(daemon.block_number + 1, current + 1, step))
+        return []
 
     def update_block_number(self, block_number):
         """Updates the internal block_number counter."""
@@ -33,27 +34,23 @@ class EventListener(metaclass=Singleton):
         daemon.block_number = block_number
         daemon.save()
 
-    def get_logs(self, block_number):
-        """Retrieves the relevant log entries from the given block.
+    def get_logs(self, from_block, to_block):
+        """Retrieves the relevant log entries from the given block range.
 
         Args:
-            block_number (int): The block number of the block to process.
+            from_block (int): The first block number.
+            to_block (int): The last block number.
 
         Returns:
             The list of relevant log entries.
         """
-        block = self.web3.eth.getBlock(block_number)
-        relevant_logs = []
-        if block and block.get('hash'):
-            for tx in block['transactions']:
-                receipt = self.web3.eth.getTransactionReceipt(tx)
-                for log in receipt.get('logs', []):
-                    if log['address'] in self.decoder.watched_addresses and \
-                            log['topics'][0] in self.decoder.topics:
-                        relevant_logs.append(log)
-            return relevant_logs, block
-        else:
-            raise UnknownBlock
+        # Note: web3.eth.getLogs is not implemented in web3 3.x
+        return self.web3.manager.request_blocking("eth_getLogs", [{
+            "fromBlock": hex(from_block),
+            "toBlock": hex(to_block),
+            "address": self.decoder.watched_addresses,
+            "topics": self.decoder.topics,
+        }])
 
     def save_events(self, decoded_logs):
         """Fires the appropriate event receivers for every given log.
@@ -69,11 +66,8 @@ class EventListener(metaclass=Singleton):
     def execute(self):
         """Program loop, does all the underlying work."""
         pending_blocks = self.get_pending_blocks()
-        for block_number in pending_blocks:
-            logs, _ = self.get_logs(block_number)
-
+        for from_block, to_block in pending_blocks:
+            logs = self.get_logs(from_block, to_block)
             decoded_logs = self.decoder.decode_logs(logs)
             self.save_events(decoded_logs)
-
-        if len(pending_blocks):
-            self.update_block_number(pending_blocks[-1])
+            self.update_block_number(to_block)
