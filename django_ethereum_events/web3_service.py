09d356f7b124368ac2ca80efa981d115ea847196
Aleksey Zhukov <drdaeman@drdaeman.pp.ru>
2017-12-19 21:35:40 +0300

    Support for Web3 4.0beta: HTTPProvider
    In Web3 3.16 the class is called RPCProvider, but in the
    upcoming 4.0 series it's replaced with HTTPProvider.
    
    This commit ensures both versions are supported in this regard.

--- django_ethereum_events/web3_service.py
+++ django_ethereum_events/web3_service.py
@@ -1,5 +1,11 @@
 from django.conf import settings
-from web3 import Web3, RPCProvider
+from web3 import Web3
+try:
+    from web3 import HTTPProvider
+    RPCProvider = None
+except ImportError:
+    from web3 import RPCProvider
+    HTTPProvider = None
 
 from .singleton import Singleton
 
@@ -15,11 +21,24 @@ class Web3Service(metaclass=Singleton):
         """
         rpc_provider = kwargs.pop('rpc_provider', None)
         if not rpc_provider:
-            rpc_provider = RPCProvider(
-                host=settings.ETHEREUM_NODE_HOST,
-                port=settings.ETHEREUM_NODE_PORT,
-                ssl=settings.ETHEREUM_NODE_SSL,
-                timeout=getattr(settings, "ETHEREUM_NODE_TIMEOUT", 10)
-            )
+            if HTTPProvider is not None:
+                uri = "{scheme}://{host}:{port}".format(
+                    host=settings.ETHEREUM_NODE_HOST,
+                    port=settings.ETHEREUM_NODE_PORT,
+                    scheme="https" if settings.ETHEREUM_NODE_SSL else "http",
+                )
+                rpc_provider = HTTPProvider(
+                    endpoint_uri=uri,
+                    timeout=getattr(settings, "ETHEREUM_NODE_TIMEOUT", 10)
+                )
+            elif RPCProvider is not None:
+                rpc_provider = RPCProvider(
+                    host=settings.ETHEREUM_NODE_HOST,
+                    port=settings.ETHEREUM_NODE_PORT,
+                    ssl=settings.ETHEREUM_NODE_SSL,
+                    timeout=getattr(settings, "ETHEREUM_NODE_TIMEOUT", 10)
+                )
+            else:
+                raise ValueError("Cannot instantiate any RPC provider")
         self.web3 = Web3(rpc_provider)
         super(Web3Service, self).__init__()
