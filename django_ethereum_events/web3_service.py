c671394f03d17e0d3621fdcfb80575568c23243c
Artemios Tomaras <tomaras@synelixis.com>
2018-05-24 17:41:43 +0300

    Added support for PoA networks

--- django_ethereum_events/web3_service.py
+++ django_ethereum_events/web3_service.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.utils.six import with_metaclass
 
 from web3 import HTTPProvider, Web3
+from web3.middleware import geth_poa_middleware
 
 from .singleton import Singleton
 
@@ -34,4 +35,9 @@ class Web3Service(with_metaclass(Singleton)):
             else:
                 raise ValueError("Cannot instantiate any RPC provider")
         self.web3 = Web3(rpc_provider)
+
+        # If running in a network with PoA consensus, inject the middleware
+        if getattr(settings, "ETHEREUM_GETH_POA", False):
+            self.web3.middleware_stack.inject(geth_poa_middleware, layer=0)
+
         super(Web3Service, self).__init__()
