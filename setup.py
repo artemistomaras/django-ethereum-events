e989949709bea718833f745f35baa566102e6a16
Artemios Tomaras <tomaras@synelixis.com>
2018-06-05 12:34:37 +0300

    Bumped version 0.2.2 -> 1.0.1

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ TEST_DEPENDENCIES = [
 
 setup(
     name='django-ethereum-events',
-    version='0.2.2',
+    version='1.0.1',
     packages=find_packages(),
     include_package_data=True,
     install_requires=DEPENDENCIES,
