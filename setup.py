2d17a293c3398984ac930ab7eb678938f83960a2
Artemios Tomaras <tomaras@synelixis.com>
2019-06-18 14:51:51 +0300

    Fixed version in setup.py

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ TEST_DEPENDENCIES = [
 
 setup(
     name='django-ethereum-events',
-    version='2.0.1',
+    version='2.0.0',
     packages=find_packages(),
     include_package_data=True,
     install_requires=DEPENDENCIES,
