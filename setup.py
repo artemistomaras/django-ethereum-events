db3870d7831288af8ce3130df815941b6c92b1c7
Artemios Tomaras <tomaras@synelixis.com>
2019-08-27 10:06:49 +0300

    Bump version 2.0.0 -> 3.0.0

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ TEST_DEPENDENCIES = [
 
 setup(
     name='django-ethereum-events',
-    version='2.0.0',
+    version='3.0.0',
     packages=find_packages(),
     include_package_data=True,
     install_requires=DEPENDENCIES,
