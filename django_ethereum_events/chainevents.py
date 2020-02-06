8ba0ece95b54252fa4d4922b0ffa1270b2a45878
Artemios Tomaras <tomaras@synelixis.com>
2020-02-06 13:43:30 +0200

    Fixed generic import

--- django_ethereum_events/chainevents.py
+++ django_ethereum_events/chainevents.py
@@ -3,10 +3,9 @@ from abc import ABCMeta, abstractmethod
 try:
     # Django 1.11.x - 2.x
     from django.utils.six import with_metaclass
-except:
+except ImportError:
     # Django 3.x
     from six import with_metaclass
-
 
 
 class AbstractEventReceiver(with_metaclass(ABCMeta)):
