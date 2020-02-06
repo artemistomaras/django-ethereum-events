b1b1f6e019228b5265c80e32ea0a7abb8f6c216d
Artemios Tomaras <tomaras@synelixis.com>
2020-02-06 12:04:53 +0200

    Fixed six import for Django 3.x

--- django_ethereum_events/chainevents.py
+++ django_ethereum_events/chainevents.py
@@ -1,6 +1,12 @@
 from abc import ABCMeta, abstractmethod
 
-from django.utils.six import with_metaclass
+try:
+    # Django 1.11.x - 2.x
+    from django.utils.six import with_metaclass
+except:
+    # Django 3.x
+    from six import with_metaclass
+
 
 
 class AbstractEventReceiver(with_metaclass(ABCMeta)):
